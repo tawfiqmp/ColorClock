a5ca7bff71dedfc850a4662dca315b7e236f9c38
Tawfiq Momin <tawfiqmp@comcast.net>
2014-10-06 08:02:08 -0500

    new upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function refreshClock ()
+{
+  var currentTime = new Date ( );
+
+//
+  var currentHours = currentTime.getHours ( );
+  var currentMinutes = currentTime.getMinutes ( );
+  var currentSeconds = currentTime.getSeconds ( );
+
+// padding seconds & minutes with zeros in the front
+  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+// string for the display
+  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+
+  // display the updated time
+  document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+
+}
